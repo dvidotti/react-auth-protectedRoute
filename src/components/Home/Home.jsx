e1898bd0dd80e35962e97e2704b8a33b26acaae6
dvidotti <danilovidotti@gmail>
2019-12-04 09:13:24 +0000

    refactor(Home):refactor home component with hooks

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,51 +1,42 @@
-import React, { Component } from 'react' 
+import React  from 'react' 
 import AuthRoute from '../Auth/auth-route'
 import './Home.css'
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loggedInUser: null
-    }
-    this.service = new AuthRoute();
-  }
-
-  logoutUser = () => {
-    this.service.logout()
+const Home = (props) => {
+  const service = new AuthRoute();
+ 
+  const logoutUser = () => {
+    service.logout()
     .then(() => {
-      this.setState({ loggedInUser: null })
-      this.props.getUser(null)
+      props.getUser(null)
     })
   }
 
-  render() {   
-    return (
-      <div>
-        <div className="full-home-page">
-          <nav className="nav">
-            {/* <img className="nav-logo" src="./images/logo.jpg" alt="ai-logo"/> */}
-            <h1 className="nav-logo">YourLogo</h1>
-            <button className='form-button' onClick={() => this.logoutUser()}>LOGOUT</button>
-          </nav>
-          <section className="section-body">
-            {this.props.loggedInUser ?
-             <h1 className="title-home">Welcome {this.props.loggedInUser.username}</h1> : <h1 className="title-home">Welcome</h1>
-            }
-            <article>
-              <h2 className="subtitle">The best title ever</h2>
-              <p className="article">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-            </article>
-            <article>
-              <h2 className="subtitle">The second best</h2>
-              <p className="article">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-            </article>
-            <footer>Your - All rights reserved 2019</footer>
-          </section>
-        </div>
+  return (
+    <div>
+      <div className="full-home-page">
+        <nav className="nav">
+          <h1 className="nav-logo">YourLogo</h1>
+          <button className='form-button' onClick={() => logoutUser()}>LOGOUT</button>
+        </nav>
+        <section className="section-body">
+          {props.loggedInUser ?
+            <h1 className="title-home">Welcome {props.loggedInUser.username}</h1> : <h1 className="title-home">Welcome</h1>
+          }
+          <article>
+            <h2 className="subtitle">The best title ever</h2>
+            <p className="article">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+          </article>
+          <article>
+            <h2 className="subtitle">The second best</h2>
+            <p className="article">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+          </article>
+          <footer>Your - All rights reserved 2019</footer>
+        </section>
       </div>
-    )
-  }
+    </div>
+  )
+  
 }
 
 export default Home;
