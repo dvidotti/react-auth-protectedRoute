133d4f6c53d86004e0fd7e0f8bd73a2d3ccc8348
dvidotti <danilovidotti@gmail>
2019-12-04 08:51:55 +0000

    refact(login):refact login with hooks

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,69 +1,53 @@
-import React, { Component }from "react"
+import React, { useState }from "react"
 import AuthRoute from './auth-route'
 import { Link } from 'react-router-dom';
 import './Auth.css'
 
-class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name:'',
-      password:'',
-      message:'',
-    }
-    this.service = new AuthRoute();
-    this.handlerChange = this.handlerChange.bind(this); 
-    this.handleFormSubmit = this.handleFormSubmit.bind(this); 
-  }
+const Login = (props) => {
+    const [name, getName] = useState('');
+    const [password, getPassword] = useState('');
+    const [message, getMessage] = useState('');
+    let service = new AuthRoute();
 
-  handlerChange(event) {
-    const { name, value } = event.target;
-    this.setState({
-      [name]:value
-    })
-  }
-
-  handleFormSubmit(event) {
+    
+  const handleFormSubmit = (event) => {
     event.preventDefault();
-    const {name, password} = this.state;
     if(name === '' || password === '') {
-      this.setState({ message: 'No user or password provided'});
+      getMessage('No user or password provided');
     } else {
-        this.service.login(name, password)
+        service.login(name, password)
         .then(response => { 
-          this.setState({name:'', password:''});
-          this.props.getUser(response)
-          this.props.history.push('./home')
+         getName('');
+         getPassword('');
+          props.getUser(response)
+          props.history.push('./home')
         })
         .catch(error => {
-          this.setState({ message: error.response.data.message});
+          getMessage(error.response.data.message);
         })
     }
   }
 
-  render() {
-    return (
-      <div className="auth-full-page">
-        <div className="auth-block">
-          <h1 className="logo">YOUR LOGO</h1>
-          {/* <img className="logo-image" src="/images/logo.jpg" alt="AI-logo"/> */}
-        </div>
-        <form className="auth-form" onSubmit={this.handleFormSubmit}>
-          <label htmlFor="name"> 
-            <input type="text" className="auth-input" name="name" placeholder="username" value={this.state.name} onChange={(e) => this.handlerChange(e)}/>
-          </label>
-          <label htmlFor="password"> 
-            <input type="password" className="auth-input" name="password"  placeholder="password" onChange={(e) => this.handlerChange(e)}></input>
-          </label>
-          <input className="auth-button" type="submit" value="Login"/>
-        </form>
-        <h1 className="error-message">{this.state.message}</h1>
-        <h4 className="conditional">Don't have an account? 
-              <Link to={"/signup"}>  SignUp  </Link>
-          </h4>
+  return (
+    <div className="auth-full-page">
+      <div className="auth-block">
+        <h1 className="logo">YOUR LOGO</h1>
       </div>
-    )
-  }
+      <form className="auth-form" onSubmit={(e) => handleFormSubmit(e)}>
+        <label htmlFor="name"> 
+          <input type="text" className="auth-input" name="name" placeholder="username" value={name} onChange={(e) => getName(e.target.value)}/>
+        </label>
+        <label htmlFor="password"> 
+          <input type="password" className="auth-input" name="password"  placeholder="password" onChange={(e) => getPassword(e.target.value)}></input>
+        </label>
+        <input className="auth-button" type="submit" value="Login"/>
+      </form>
+      <h1 className="error-message">{message}</h1>
+      <h4 className="conditional">Don't have an account? 
+            <Link to={"/signup"}>  SignUp  </Link>
+        </h4>
+    </div>
+  )
 }
 
 export default Login;
